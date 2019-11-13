b26d2d63b8fe38e2c5c62f3348b9284bffe4ce11
Luuk-Pasker <luuk.pas@gmail.com>
2019-11-13 08:54:24 +0100

    First commit website, added db_config, header and footer

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$databasename = "wideworldimporters";
+$user = "root";
+$pass = ""; //eigen password invullen
+$port = 3306;
+$connection = mysqli_connect($host, $user, $pass, $databasename, $port);
---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php
+    include "db_config.php";
+    ?>
+    <title>
+        World Wide Importers
+    </title>
+</head>
+<body>
