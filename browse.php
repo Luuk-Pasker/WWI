d23a85e04a78de8dff368d50d400fa45c314e6c1
Luuk-Pasker <luuk.pas@gmail.com>
2019-11-13 11:26:43 +0100

    Added categories version 0.1

--- browse.php
+++ browse.php
@@ -1,24 +1,56 @@
 <?php
 include "includes/header.php";
 
+$sql = "SELECT * FROM stockgroups ORDER BY StockGroupName";
+$result = mysqli_query($connection, $sql);
+
+print("<form method='post'>");
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+{
+    print("<button type='submit' name='id' value='" . $row['StockGroupID'] . "'>" . $row['StockGroupName'] . "</button>");
+}
+print("</form>");
+
+if(isset($_POST['id'])) {
+    $id = $_POST['id'];
+}
+
+
+
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
 } else {
     $page = 1;
 }
+
 $no_of_records_per_page = 25;
 $offset = ($page-1) * $no_of_records_per_page;
 
-$total_pages_sql = "SELECT COUNT(*) FROM stockitems";
+
+if (isset($_POST['id'])) {
+    $total_pages_sql = "SELECT COUNT(DISTINCT StockItemID) FROM stockitemstockgroups WHERE StockGroupID = $id";
+} else {
+    $total_pages_sql = "SELECT COUNT(DISTINCT StockItemID) FROM stockitemstockgroups";
+}
+
 $result = mysqli_query($connection,$total_pages_sql);
 $total_rows = mysqli_fetch_array($result)[0];
 $total_pages = ceil($total_rows / $no_of_records_per_page);
 
-$sql = "SELECT * FROM stockitems si JOIN stockitemholdings sih ON si.StockItemId = sih.StockItemId ORDER BY si.StockItemName LIMIT $offset, $no_of_records_per_page";
+if (isset($_POST['id'])){
+    $sql = "SELECT * FROM stockitemstockgroups sisg JOIN stockitems si ON sisg.StockItemID = si.StockItemID JOIN stockitemholdings sih ON si.StockItemId = sih.StockItemId WHERE sisg.StockGroupID = $id ORDER BY si.StockItemName LIMIT $offset, $no_of_records_per_page";
+} else {
+    $sql = "SELECT * FROM stockitemstockgroups sisg JOIN stockitems si ON sisg.StockItemID = si.StockItemID JOIN stockitemholdings sih ON si.StockItemId = sih.StockItemId ORDER BY si.StockItemName LIMIT $offset, $no_of_records_per_page";
+}
+
+
 $res_data = mysqli_query($connection,$sql);
 while($row = mysqli_fetch_array($res_data)){
+
     print($row["StockItemName"] . " €" . $row["UnitPrice"] . " Aantal:" . $row["QuantityOnHand"] . "<br>");
+
 }
+
 mysqli_close($connection);
 ?>
 <ul class="pagination">
