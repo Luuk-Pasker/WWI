57e075216b5a94cba65f580ef4fc34f3fb57b73d
Davy <davyvanzalk@gmail.com>
2019-11-13 12:30:33 +0100

    product bekijken heeft nu een maximum aantal wat besteld kan worden = )

--- productBekijken.php
+++ productBekijken.php
@@ -6,30 +6,34 @@ include "includes/header.php";
 <?php
 $ItemID = 120;
 
+//Dit vraagt de gegevens van een gegeven ItemID
 $sql = "SELECT * FROM stockitems S 
         LEFT JOIN stockitemholdings H ON S.StockItemID = H.StockItemID 
         WHERE S.StockItemID = $ItemID";
 $result = mysqli_query($connection, $sql);
 
 
-// loop langs alle rijen
+//
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
+    $voorraad = $row["QuantityOnHand"];
     print($row["StockItemName"] . "<br>");
+    print("<img src='images/120_dino_slippers.jpg'" . $row["Photo"] . "<br>");
     print("€" . $row["UnitPrice"] . "<br>");
     print($row["SearchDetails"] . "<br>");
-    print("Nog " . $row["QuantityOnHand"] . " op voorraad.<br>");
-    print("<img src='images/120_dino_slippers.jpg'" . $row["Photo"] . "<br>");
 }
 
-
-print("<form>");
-print("Aantal:");
-print('<input type="number" name="quantity" min="1" max=<?php$row["QuantityOnHand"]?>');
-print('<input type="submit" value="Toevoegen aan winkelmand">');
-print("</form>");
-
+print("Nog " . $voorraad . " op voorraad.<br>");
 ?>
+
+<form>
+    Aantal:
+    <input type="number" name="quantity" min="1" max="<?php print("$voorraad"); ?>">
+    <input type="submit" value="Toevoegen aan winkelmand">
+</form>
+
+
+
 <?php
 include "includes/footer.php";
 ?>
