60120f0956defd11d2d65bb5f3beac8bf853d676
Luuk-Pasker <luuk.pas@gmail.com>
2019-11-13 09:52:36 +0100

    Added css file with content for header, added menu bar

--- includes/header.php
+++ includes/header.php
@@ -7,5 +7,14 @@
     <title>
         World Wide Importers
     </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+<div class="header">
+    <a href="#default" class="logo">CompanyLogo</a>
+    <div class="header-right">
+        <a class="active" href="index.php">Home</a>
+        <a href="browse.php">Browse</a>
+        <a href="#about">About</a>
+    </div>
+</div>
