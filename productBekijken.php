8c460538e2f590c4c8fcb4957c756b32576dae98
Username <email@address.com>
2019-11-14 11:04:02 +0100

    productbekijken 3.1 (engeltalig gemaakt)

--- productBekijken.php
+++ productBekijken.php
@@ -31,10 +31,10 @@
 
                     print("<div class='test'>");
                     print("<div class='naam'>" . $row["StockItemName"] . "</div><br>");
-                    print("<div class='beschrijving2'>Beschrijving:</div><BR>" ."<div class='beschrijving'>" . $row["SearchDetails"] . "</div><br>");
+                    print("<div class='beschrijving2'>Discription:</div><BR>" ."<div class='beschrijving'>" . $row["SearchDetails"] . "</div><br>");
                     print("<div class='prijs'>" . "€" . $row["UnitPrice"] . "</div><br>");
                     $voorraad = $row["QuantityOnHand"];
-                    print("<div class='nogopvoorraad'>" . "Nog " . $voorraad . " op voorraad.</div><br>");
+                    print("<div class='nogopvoorraad'>" . $voorraad . " in stock.</div><br>");
                     print("</div>");
 
 
@@ -46,17 +46,12 @@
                <!aantallen selecteer formulee/toevoegen aan winkelmand knop>
                 <div class="aantallen">
                     <form>
-                        <input id="toevoegenaanwinkelmand" type="submit" value="Toevoegen aan winkelmand">
-                        Aantal: <input id="aantalx" type="number" name="quantity" min="1" max="<?php print("$voorraad"); ?>">
+                        <input id="toevoegenaanwinkelmand" type="submit" value="add to shopping cart">
+                        Amount: <input id="aantalx" type="number" name="quantity" min="1" max="<?php print("$voorraad"); ?>">
                     </form>
 
                 </div>
                 <!>
-
-                <?php
-/*                print("Nog " . $voorraad . " op voorraad.<br>");
-                */?>
-
 
                 <!footer>
                 <?php
