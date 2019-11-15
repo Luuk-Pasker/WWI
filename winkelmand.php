00e998c96ac4eaf7e5490fba34582673df9a6c99
EvaStienstra <e.stienstra@live.nl>
2019-11-15 14:01:34 +0100

    css styling for h2 shoppingcart

--- winkelmand.php
+++ winkelmand.php
@@ -3,7 +3,7 @@ include "includes/header.php";
 ?>
 
 <a href="index.php">< Go back to shopping</a>
-<h2>shoppingcart</h2>
+<h2 class="a.hoofdtext"> shoppingcart</h2>
 
 <?php
 include "includes/footer.php";
