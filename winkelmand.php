33c196c4b6da4f6155ce8a318ae886a50a595c79
kayleigh22 <kayleigh22x@gmail.com>
2019-11-18 12:05:56 +0100

    about us, cssshit

--- winkelmand.php
+++ winkelmand.php
@@ -4,7 +4,7 @@ include "includes/header.php";
 ?>
 
 <a href="index.php">< Go back to shopping</a>
-<h2 class="a.hoofdtext"> shoppingcart</h2>
+<h2 class="a.hoofdtext"> Shoppingcart</h2>
 
 <?php
 include "includes/footer.php";
