ba5f9c185ba8941b91daaba00ad44bae0b561536
kayleigh22 <kayleigh22x@gmail.com>
2019-11-19 21:40:57 +0100

    guarantee and about us english text

--- guarantee.php
+++ guarantee.php
@@ -2,4 +2,20 @@
 $active = "about";
 include "includes/header.php";
 ?>
-<div class="tekstabout"><h1><tekst class="tekstabout"></tekst>Guerantee</h1></tekst></div>
+<link rel="stylesheet" type="text/css" href = "css/aboutus.css">
+<button class='button' type="submit">Contact Form</button>
+<a href="guarantee.php" ><button class='button' type="submit">Guarantee </button></a>
+<link rel="stylesheet" type="text/css" href = "css/aboutus.css">
+<div class="tekstabout"><h1><tekst class="tekstabout"></tekst>Guarantee</h1></tekst></div>
+<br>
+<br>
+<br>
+<br>
+
+<div class="guarantee"><h4>We guarantee that if you buy an item from WWI it is in good condition and continues to work as expected under normal use, this is the legal guarantee. If there are signs of misuse of the product than it was meant for, this warranty does not apply.
+    If there is something wrong with the item, we will look for a good solution. Depending on your item, we will arrange a reparation or repayment of the purchase amount.<br>
+    <br>Your invoice or proof of purchase is also your proof of warranty.<br>
+    <br>How long the guarantee will obtain depends on the item. The guarantee that we offer is legally established and applies to the product durability of an item. One item has a longer durability than the other.
+    <br>There’s a dissimilarity between every factory with the warranty of the items. You can see how long that lasts in the captions of your article.<br>
+        <br> If there are any questions or problems about your product, go to contact form to get in touch with us.
+</h4></div>
