24b6e6787b6bb39ab3bb1af0434407b1390b1c29
Davy <davyvanzalk@gmail.com>
2019-11-19 12:43:35 +0100

    no message

---
+++ includes/funtions.php
@@ -0,0 +1,72 @@
+<?php
+
+function print_sc_head(){
+    print("<div class='scRow'>");
+    print("<div class='scImage'>");
+    print("</div>");
+    print("<div class='scName'>");
+    print("<b>Item name</b>");
+    print("</div>");
+    print("<div class='scAmount'>");
+    print("<b>Amount</b>");
+    print("</div>");
+    print("<div class='scPrice'>");
+    print("<b>Price per item</b>");
+    print("</div>");
+    print("<div class='scTPrice'>");
+    print("<b>Total price</b>");
+    print("</div>");
+    print("</div>");
+}
+
+function print_sc_item($index){
+    if($_SESSION["Quantitys"][$index]>0) {
+        print("<div class='scRow'>");
+        print("<div class='scImage'>");
+        $Photo = $_SESSION['Images'][$index];
+        print("<img src='$Photo' width='100px'>");
+        print("</div>");
+        print("<div class='scName'>");
+        print($_SESSION["Names"][$index]);
+        print("</div>");
+        print("<div class='scAmount'>");
+        print($_SESSION["Quantitys"][$index]);
+        print("</div>");
+        print("<div class='scPrice'>");
+        print("€" . number_format((float)$_SESSION["Prices"][$index], 2, '.', ''));
+        print("</div>");
+        print("<div class='scTPrice'>");
+        $TPrice = $_SESSION["Prices"][$index] * $_SESSION["Quantitys"][$index];
+        print("€" . number_format((float)$TPrice, 2, '.', ''));
+        $_SESSION["TotalPrice"] += $TPrice;
+        print("</div>");
+        print("</div>");
+    }
+}
+
+function print_sc_foot(){
+    if($_SESSION["TotalPrice"]<50) {
+        $shippingPrice = 6.95;
+    }else{
+        $shippingPrice = 0;
+    }
+    print("<div class='scRow'>");
+    print("<div class='scImage'>");
+    print("</div>");
+    print("<div class='scName'>");
+    print("shipping price is €6.95 for orders below €50.00");
+    print("</div>");
+    print("<div class='scAmount'>");
+    print("</div>");
+    print("<div class='scPrice'>");
+    print("<b>Shipping price:</b><br><br><b>Total price:</b>");
+    print("</div>");
+    print("<div class='scTPrice'>");
+    print("<b>€" . number_format((float)$shippingPrice, 2, '.', '') . "</b><br><br>");
+    $_SESSION["TotalPrice"] += $shippingPrice;
+    print("<b>€" . number_format((float)$_SESSION["TotalPrice"], 2, '.', '') . "</b>");
+    print("</div>");
+    print("</div>");
+
+    print("<form action='betalen.php' class='scButton'><input type='submit' value='Buy items'></form>");
+}
