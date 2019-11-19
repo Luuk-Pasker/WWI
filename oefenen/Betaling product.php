52a70bc0d31c7dd14e6961d90b788f3f629e2312
EvaStienstra <e.stienstra@live.nl>
2019-11-19 12:40:57 +0100

    betaling product

---
+++ oefenen/Betaling product.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Betaling product</title>
+    <link rel="stylesheet" type="text/css" href = "css/betaling.css">
+
+</head>
+<body>
+
+<h2> Payment </h2> <br> <br>
+
+<h4> Information </h4>
+<form method="$_GET"  action="Betaling%20product.php">
+    <div>
+    E-mail adres: <input type="email" name="email">
+    </div>
+    <div>
+    Sign up for the newspaper:<input type ="checkbox" name="Sign up">
+    </div>
+</form>
+E-mail adress
+
+<?php
