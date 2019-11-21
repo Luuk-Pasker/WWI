ae8fb7f9ea4f147771c1a74c7330079da47d9e2d
Luuk-Pasker <luuk.pas@gmail.com>
2019-11-21 13:36:28 +0100

    Changed dashboard, its now personal! :D

--- login.php
+++ login.php
@@ -1,45 +1,47 @@
 <?php
 $active = "login";
 include "includes/header.php";
+
 ?>
 
-<div class="loginBox">
-    <form method="post">
-        <div class="loginRow">
-            <div class="loginHead">
-                <h2>Wide World Importers Login</h2>
+    <div class="loginBox">
+        <form method="post">
+            <div class="loginRow">
+                <div class="loginHead">
+                    <h2>Wide World Importers Login</h2>
+                </div>
             </div>
-        </div>
-        <div class="loginRow">
-            <div class="loginColumn1">
-                <label for="inputEmail" class=""><div class ='email'>Email: </div></label>
+            <div class="loginRow">
+                <div class="loginColumn1">
+                    <label for="inputEmail" class="">Username:</label>
+                </div>
+                <div class="loginColumn2">
+                    <input type="text" class="loginInput" id="inputEmail" name="username" placeholder="Username">
+                </div>
             </div>
-            <div class="loginColumn2">
-                <input type="text" class="loginInput" id="inputEmail" name="username" placeholder="Email">
+            <div class="loginRow">
+                <div class="loginColumn1">
+                    <label for="inputPassword">Password:</label>
+                </div>
+                <div class="loginColumn2">
+                    <input type="password" class="loginInput" id="inputPassword" name="password" placeholder="Password">
+                </div>
             </div>
-        </div>
-        <div class="loginRow">
-            <div class="loginColumn1">
-                <label for="inputPassword" class="">Password:</label>
+            <div class="loginRow">
+                <div class="loginColumn2">
+                    <input type="submit" class="" name="submit" value="Sign in"> <a class="loginLink" href="">Forgot
+                        password?</a> <!-- sign in en forgot password doen nog niks -->
+                </div>
             </div>
-            <div class="loginColumn2">
-                <input type="password" class="loginInput" id="inputPassword" name="password" placeholder="Password">
-            </div>
-        </div>
-        <div class="loginRow">
-            <div class="loginColumn2">
-                <input type="submit" class="" name="submit" value="Sign in"> <a class="loginLink" href="">Forgot password?</a> <!-- sign in en forgot password doen nog niks -->
-            </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 
 <?php
 $username = "";
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     if (empty($_POST['username']) && empty($_POST['password'])) {
-        echo "Fill in data";
+        echo "Fill in data - ";
     } else {
         $username = $_POST['username'];
         $HPass = hash('sha256', $_POST['password']);
@@ -52,16 +54,16 @@ if(isset($_POST['submit'])) {
     while ($row = mysqli_fetch_array($res_data)) {
         if ($row['LogonName'] == $username && $row['HashedPassword'] == $HPass) {
             $_SESSION['login'] = TRUE;
-            echo "Ingelogt";
+            $_SESSION['user_session'] = $row['PersonID'];
         } else {
             $_SESSION['login'] = FALSE;
-            echo "Verkeerde gegevens!";
+            echo "Wrong data!";
         }
     }
-    if ($_SESSION['login'] == TRUE){
-        echo '<script> window.location.href = "dashboard.php"; </script>';
-        echo "ingelogt!";
-    }
+}
+
+if ($_SESSION['login'] == TRUE) {
+    echo '<script> window.location.href = "admin/dashboard.php"; </script>';
 }
 
 include "includes/footer.php";
