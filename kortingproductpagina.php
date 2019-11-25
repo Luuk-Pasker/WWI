0241c8d57f49ae43101bed796ce88190b950446a
Username <email@address.com>
2019-11-25 14:45:05 +0100

    kortingproductpagina

---
+++ kortingproductpagina.php
@@ -0,0 +1,25 @@
+<?php
+include "includes/funtions.php";
+
+$host = "localhost";
+$databasename = "wideworldimporters";
+$user = "root";
+$pass = ""; //eigen password invullen
+$port = 3306;
+$connection = mysqli_connect($host, $user, $pass, $databasename, $port);
+
+GetDeals($connection);
+
+$results = GetDeals($connection);
+$aantalpaginas = floor(mysqli_num_rows($results) / 3);
+$fullarray = array();
+$i = 0;
+foreach ($results as $row) {
+    if (mysqli_num_rows($results) != 0) {
+        $fullarray[$i] = $row;
+        $i++;
+
+    }
+}
+
+print($fullarray[2]['SpecialDealID']);
