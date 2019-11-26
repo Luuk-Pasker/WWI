f5eba96443e6c235babc34b29b27e66c72b1f32b
EvaStienstra <e.stienstra@live.nl>
2019-11-26 14:56:53 +0100

    Betalen update (3.0)

--- betalen 2.0.php
+++ betalen 2.0.php
@@ -1,8 +1,30 @@
-<?php
-print("Hi how u doing");
-?>
-<?php
 
+<?php
+active:"betalen2";
+include "includes/header.php";
+?>
+
+
+
+<a href="betalen.php"> back to step 1 and 2 </a>
+
+<h3 class="titlepayment"> Paymentmethod</h3>
+<form method="POST" action="afgeronde_bestelling.php">
+    <div class="Payment">
+    <input type="radio" name="select"> Cash <br> <br>
+    <input type="radio" name="select"> Check <br><br>
+    <input type="radio" name="select"> Credit-Card <br><br>
+    <input type="radio" name="select"> EFT  <br>
+    I have a discount code: <input type="textarea" name="discount"><br>
+        <input type="submit" value="Payment">
+</form>
+    <input type="submit" value="Enter">
+
+
+</div>
+
+
+<?php
 /*foreach ($_SESSION["IDs"] as $i => $value) {
     unset($_SESSION["IDs"][$i]);
 }
@@ -20,4 +42,5 @@ foreach ($_SESSION["Quantitys"] as $i => $value) {
 }
 $_SESSION["TotalPrice"] = 0;
 
-*/?>
+*/
+?>
