e7c80489393fc8d9565525373b2786ffc2ac378a
EvaStienstra <e.stienstra@live.nl>
2019-11-28 11:02:22 +0100

    Betalen update 5.0

--- oefenen/Betalen/bezig.php
+++ oefenen/Betalen/bezig.php
@@ -7,9 +7,9 @@ include "includes/header.php";
 <?php
 $Password1=isset($_POST['password']);
 $Passwordagain=isset($_POST['passwordagain']);
-$Username=isset($_POST['username']);
+$Username1=isset($_POST['username']);
 
-if(!$Username) {
+if(!$Username1) {
     print("No username/password");
 } if($Password1 != $Passwordagain){
     print("The password is not the same");
@@ -18,9 +18,16 @@ if(!$Username) {
 }
 ?>
 <form method = "$_POST" action="bezig.php">
-Username = <input type="text" name="usenname"> <br>
-Password = <input type="password" name="password"><br>
-Password (again) = <input type="password" name="passwordagain"> <br> <br>
+    <label for="user" class="usernamebetalen"> Username = </label>
+    <input type="text" id="user" name="usenname">
+    <br>
+    <label for="pass" class="passwordbetalen"> Password = </label>
+    <input type="password" id="pass" name="password">
+    <br>
+    <label for="pass1" class="passfirst"> Password (again) = </label>l
+    <input type="password" name="passwordagain">
+    <br>
+    <br>
 <input type="submit" name="inloggen" value="Log in">
 </form>
 
@@ -32,16 +39,33 @@ $totalPrice = print($_SESSION["TotalPrice"]);
 <h3> Total </h3> <!--rechterkant en border-->
 
 Total: €<?php $totalPrice ?>
+<br>
 * with sendingcosts included
 
 
 <h3> Your Order </h3> <!-- border + border om elk product > plaatje Float; right overflow: scroll;-->
+
+<label for="aantal" class="aantalproducten"> Aantal: </label>
+<input type="text" id="aantal" name="aantalproductenbetalen">
+}
+
 <?php
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
-    $_SESSION["Names"];
-$_SESSION["Quantitys"];
-$_SESSION["Prices"];
-$image = "images/" . $row['Photo'];
+foreach($_SESSION["IDs"] as $index => $val) {
+$Photo = $_SESSION['Images'][$index];
+$ItemPrice = $_SESSION["Prices"][$index] * $_SESSION["Quantitys"][$index];
+print("<img src='$Photo' height='100px'>");
+print("<br>");
+print($_SESSION["Names"][$index]);
+print("<br>");
+print($_SESSION["Quantitys"][$index]);
+print("<br>");
+print("€" . number_format((float)$ItemPrice, 2, '.', ''));
 
 
+/*while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+    print($_SESSION["Names"]);
+print($_SESSION["Quantitys"]);
+print($_SESSION["Prices"]);
+print($_SESSION["Images"]);*/
 ?>
+
