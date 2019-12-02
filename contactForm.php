2ded1dc4131dae7db5c3e79688c186c44dd1717a
Luuk-Pasker <luuk.pas@gmail.com>
2019-12-02 11:22:36 +0100

    Can now use contact form

--- contactForm.php
+++ contactForm.php
@@ -5,8 +5,12 @@ include "includes/header.php";
 <!--fdjsi-->
 
 <link rel="stylesheet" type="text/css" href="css/aboutus.css" xmlns="">
-<a href="contactForm.php" ><button class="button" type="submit">Contact Form</button> </a>
-<a href="guarantee.php" ><button class='button' type="submit">Warranty </button></a>
+<a href="contactForm.php">
+    <button class="button" type="submit">Contact Form</button>
+</a>
+<a href="guarantee.php">
+    <button class='button' type="submit">Warranty</button>
+</a>
 
 <div class="row">
     <div class="column">
@@ -32,58 +36,86 @@ include "includes/header.php";
 
             <h2>Contact form</h2>
 
-            <div class="contact-form-group">
-                <label for="subject" class="contact-form-label">Select your subject:</label>
-                <select id="subject" class="contact-form-control">
-                    <optgroup label="Choose subject"</comment>
-                    <option value="Delivery">Delivery</option>
-                    <option value="General">General question</option>
-                    <option value="Guarantee">Guarantee</option>
-                    <option value="Order">Order</option>
-                </select>
-            </div>
+            <form method="post">
+                <div class="contact-form-group">
+                    <label for="subject" class="contact-form-label">Select your subject:</label>
+                    <select id="subject" name="subject" class="contact-form-control">
+                        <optgroup label="Choose subject"
+                        </comment>
+                        <option value="Delivery">Delivery</option>
+                        <option value="General">General question</option>
+                        <option value="Guarantee">Guarantee</option>
+                        <option value="Order">Order</option>
+                    </select>
+                </div>
 
-            <div class="contact-form-group">
-                <label for="message" class="contact-form-label">Message/Question:</label>
-                <textarea rows="4" cols="50" id="message" class="contact-form-control"></textarea>
-            </div>
-<br>
-            <br>
-            <h2>Personal information</h2>
+                <div class="contact-form-group">
+                    <label for="message" class="contact-form-label">Message/Question:</label>
+                    <textarea rows="4" cols="50" id="message" name="message" class="contact-form-control"></textarea>
+                </div>
+                <br>
+                <br>
+                <h2>Personal information</h2>
 
-            <div class="contact-form-group">
-            <label for="salutation" class="contact-form-label">Salutation:</label>
-            <select id="salutation" class="contact-form-control">
-                <option value="Mr">Mr.</option>
-                <option value="Ms">Ms.</option>
-            </select>
-            </div>
+                <div class="contact-form-group">
+                    <label for="salutation" class="contact-form-label">Salutation:</label>
+                    <select id="salutation" class="contact-form-control">
+                        <option value="Mr">Mr.</option>
+                        <option value="Ms">Ms.</option>
+                    </select>
+                </div>
 
-            <div class="contact-form-group">
-            <label for="first_name" class="contact-form-label">First name:</label>
-            <input type="text" id="first_name" class="contact-form-control"/>
-            </div>
+                <div class="contact-form-group">
+                    <label for="first_name" class="contact-form-label">First name:</label>
+                    <input type="text" id="first_name" name="fname" class="contact-form-control"/>
+                </div>
 
-            <div class="contact-form-group">
-            <label for="last_name" class="contact-form-label">Last name:</label>
-            <input type="text" id="last_name" class="contact-form-control"/>
-            </div>
-<br>
-            <h2>Contact details</h2>
+                <div class="contact-form-group">
+                    <label for="last_name" class="contact-form-label">Last name:</label>
+                    <input type="text" id="last_name" name="lname" class="contact-form-control"/>
+                </div>
+                <br>
+                <h2>Contact details</h2>
 
-            <div class="contact-form-group">
-            <label for="email" class="contact-form-label">Email:</label>
-            <input type="email" id="email" class="contact-form-control"/>
-            </div>
+                <div class="contact-form-group">
+                    <label for="email" class="contact-form-label">Email:</label>
+                    <input type="email" id="email" name="email" class="contact-form-control"/>
+                </div>
 
-            <div class="contact-form-group">
-            <label for="phone_number" class="contact-form-label">Phone number:</label>
-            <input type="text" id="phone_number" class="contact-form-control"/>
-            </div>
-            <br>
-            <br>
-            <br>
-            <input type="submit" value="Send"/>
+                <div class="contact-form-group">
+                    <label for="phone_number" class="contact-form-label">Phone number:</label>
+                    <input type="number" id="phone_number" name="phone" class="contact-form-control"/>
+                </div>
+                <br>
+                <br>
+                <br>
+                <input type="submit" name="submit" value="Send"/>
+
+            </form>
+
+            <?php
+            if (isset($_POST['submit'])) {
+                if (empty($_POST['message'])) {
+                    echo "Fill in your message!";
+                } elseif (empty($_POST['fname'])) {
+                    echo "Fill in your first name!";
+                } elseif (empty($_POST['lname'])) {
+                    echo "Fill in your last name!";
+                } elseif (empty($_POST['email'])) {
+                    echo "Fill in email address!";
+                } elseif (empty($_POST['phone'])) {
+                    echo "Fill in phone number!";
+                }else {
+                    $name = $_POST['fname'] . " " . $_POST['lname'];
+                    $sql = "INSERT INTO contact (subject, name, email, phoneNumber, message) VALUES (?, ?, ?, ?, ?)";
+
+                    $stmt = $connection->prepare($sql);
+                    $stmt->bind_param('sssss', $_POST['subject'], $name, $_POST['email'], $_POST['phone'], $_POST['message']);
+                    $stmt->execute();
+                    echo "Your message has been send!";
+                }
+            }
+            ?>
 
         </div>
     </div>
