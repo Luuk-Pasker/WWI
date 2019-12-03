47457bd8c3605fe5a1307d0c25813c7870f36397
EvaStienstra <e.stienstra@live.nl>
2019-12-03 09:50:52 +0100

    Bevestiging

--- Bevestiging.php
+++ Bevestiging.php
@@ -17,3 +17,33 @@ include "includes/header.php"
 </p>
 </div>
 
+<?php
+/*error_reporting(E_ALL);
+$ontvanger = "mijn emailu";
+$hoofd = "Contactformulier";
+$naam = $_POST['naam'];
+$telefoon = $_POST['telefoon'];
+$email = $_POST['email'];
+$onderwerp = $_POST['onderwerp'];
+$bericht = $_POST['bericht'];
+// body voor de email opmaken
+$body = "";
+$body .= "Naam: ".$naam. "\n";
+$body .= "Telefoon: ".$telefoon. "\n";
+$body .= "Email: ".$email. "\n";
+$body .= "Onderwerp: ".$onderwerp. "\n";
+$body .= "Bericht: ". $bericht. "\n";
+// email verzenden
+$formsent = mail($ontvanger, $hoofd, $body, "From: <$email>");
+$replyemail = mail($onderwerp2,$bericht2,"From: <$email>:");//" vergeten en body zit hier niet in
+$body .= "<mijn email>\n";
+$body2 .= "X-Sender: <null>\n";
+$headers2 .= "Return-Path: <null>\n";
+$onderwerp2 .= "Bedankt u voor uw reactie";
+$bericht2 .= "Beste $naam,
+Bedankt voor uw reactie! Ik neem zo spoedig mogelijk contact met u op via uw e-mailadres, $replyemail .
+Groeten,
+rene ); //hier zit nog niet alles snor
+}";
+*/?>
+
