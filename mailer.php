66a95d914a547cf66e878d2d92c5b6608ad7dd72
kayleigh22 <kayleigh22x@gmail.com>
2019-12-09 15:06:14 +0100

    php mailer

---
+++ mailer.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'C:\xampp\composer\vendor\autoload.php';
+
+/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+$mail = new PHPMailer(TRUE);
+
+/* Open the try/catch block. */
+try {
+    /* Set the mail sender. */
+    $mail->setFrom('darth@empire.com', 'Darth Vader');
+
+    /* Add a recipient. */
+    $mail->addAddress('palpatine@empire.com', 'Emperor');
+
+    /* Set the subject. */
+    $mail->Subject = 'Force';
+
+    /* Set the mail message body. */
+    $mail->Body = 'There is a great disturbance in the Force.';
+
+    /* Finally send the mail. */
+    $mail->send();
+}
+catch (Exception $e)
+{
+    /* PHPMailer exception. */
+    echo $e->errorMessage();
+}
+catch (\Exception $e)
+{
+    /* PHP exception (note the backslash to select the global namespace Exception class). */
+    echo $e->getMessage();
+}
