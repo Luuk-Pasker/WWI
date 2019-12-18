f1537c386ad5be35376182880ef2e6ddb1d35730
kayleigh22 <kayleigh22x@gmail.com>
2019-12-18 09:14:58 +0100

    Gefixt door Luuk, aantal word aangepast

--- betalen.php
+++ betalen.php
@@ -288,7 +288,6 @@ $result3 = mysqli_query($connection, $discount);
                     echo $id;
                     echo $Quantity;
 
-
                     $sql = "SELECT * FROM stockitemholdings WHERE StockItemID = '$id'";
                     /*printen van de resultaten op het scherm*/
                     $res_data = mysqli_query($connection, $sql);
@@ -303,7 +302,6 @@ $result3 = mysqli_query($connection, $discount);
                         $stmt->execute();
 
                     }
-
 
                     $sql = "SELECT MAX(InvoicesID) AS HighestID FROM invoice";
                     /*printen van de resultaten op het scherm*/
@@ -369,6 +367,21 @@ $result3 = mysqli_query($connection, $discount);
                         echo $id;
                         echo $Quantity;
 
+                        $sql = "SELECT * FROM stockitemholdings WHERE StockItemID = '$id'";
+                        /*printen van de resultaten op het scherm*/
+                        $res_data = mysqli_query($connection, $sql);
+                        foreach ($res_data as $row) {
+                            $minQuantity = $row['QuantityOnHand'] - $Quantity;
+                            echo $minQuantity;
+
+                            $sql = "UPDATE stockitemholdings SET QuantityOnHand = ?, LastStocktakeQuantity = ? WHERE StockItemID = '$id'";
+
+                            $stmt = $connection->prepare($sql);
+                            $stmt->bind_param('ss', $minQuantity, $minQuantity);
+                            $stmt->execute();
+
+                        }
+
                         $sql = "SELECT MAX(InvoicesID) AS HighestID FROM invoice";
                         /*printen van de resultaten op het scherm*/
                         $res_data = mysqli_query($connection, $sql);
@@ -409,6 +422,21 @@ $result3 = mysqli_query($connection, $discount);
                         $Quantity = $_SESSION['Quantitys'][$index];
                         echo $id;
                         echo $Quantity;
+
+                        $sql = "SELECT * FROM stockitemholdings WHERE StockItemID = '$id'";
+                        /*printen van de resultaten op het scherm*/
+                        $res_data = mysqli_query($connection, $sql);
+                        foreach ($res_data as $row) {
+                            $minQuantity = $row['QuantityOnHand'] - $Quantity;
+                            echo $minQuantity;
+
+                            $sql = "UPDATE stockitemholdings SET QuantityOnHand = ?, LastStocktakeQuantity = ? WHERE StockItemID = '$id'";
+
+                            $stmt = $connection->prepare($sql);
+                            $stmt->bind_param('ss', $minQuantity, $minQuantity);
+                            $stmt->execute();
+
+                        }
 
                         $sql = "SELECT MAX(InvoicesID) AS HighestID FROM invoice";
                         /*printen van de resultaten op het scherm*/
